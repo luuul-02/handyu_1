d7c7d033fad909a7b3f1c5b770e573030b793f70
androidstudio <dds04260@gmail.com>
2020-11-22 10:36:28 +0900

    이미지 url 다운로드 실패

--- app/src/main/java/com/example/handyu_1/MainActivity.java
+++ app/src/main/java/com/example/handyu_1/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.handyu_1;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -14,8 +15,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.module.AppGlideModule;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -35,7 +39,7 @@ import java.util.Calendar;
 public class MainActivity extends AppCompatActivity {
 
     Button txtview_day;
-    ImageView imgview_sliberal,imgview_snatural;
+    ImageView imgview_snatural,imgview_stfnatural;//imgview_sliberal,imgview_stfliberal;
 
     long now = System.currentTimeMillis();
     Date mdate = new Date(now);
@@ -43,68 +47,53 @@ public class MainActivity extends AppCompatActivity {
     SimpleDateFormat simpleDate = new SimpleDateFormat("yy-MM-dd E요일", Locale.KOREAN);
     String getTime = simpleDate.format(mdate);
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         txtview_day = (Button) findViewById(R.id.txtview_day);
-        imgview_sliberal = (ImageView) findViewById(R.id.imgview_sliberal);
         imgview_snatural = (ImageView) findViewById(R.id.imgview_snatural);
+        imgview_stfnatural = (ImageView) findViewById(R.id.imgview_stfnatural);
+//        imgview_sliberal = (ImageView) findViewById(R.id.imgview_sliberal);
+//        imgview_stfliberal = (ImageView) findViewById(R.id.imgview_stfliberal);
 
-//        // Cloud Storage 설정
-//        FirebaseStorage storage = FirebaseStorage.getInstance();
-//        // storage 주소설정
-//        StorageReference storageRef = storage.getReference();
-//        //다운로드할 파일을 가르키는 참조 만들기
-//        StorageReference pathReference = storageRef.child("natural/");
-//        // Create a reference to a file from a Google Cloud Storage URI
-//        StorageReference gsReference = storage.getReferenceFromUrl("gs://handyu1.appspot.com/natural");
 
+        //FirebaseStorage 인스턴스를 생성
         FirebaseStorage storage = FirebaseStorage.getInstance();
-        StorageReference storageRef = storage.getReference().child("gs://handyu1.appspot.com");
-        StorageReference pathRef = storageRef.child("natural/20201110055413_natural_staff.jpg");
-        pathRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-            @Override
-            public void onSuccess(Uri uri) {
-                //이미지 로드 성공시
 
-                Toast.makeText(getApplicationContext(), "성공", Toast.LENGTH_SHORT).show();
-//                Glide.with(getApplicationContext())
-//                        .load(uri)
-//                        .into(imgview_snatural);
+        StorageReference storageRef_studentN = storage.getReference("natural/20201110055413_natural_student.jpg");
+        StorageReference storageRef_staffN = storage.getReference("natural/20201110055413_natural_staff.jpg");
 
-            }
-        }).addOnFailureListener(new OnFailureListener() {
+        //StorageReference에서 파일 다운로드 URL 가져옴
+        storageRef_studentN.getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
             @Override
-            public void onFailure(@NonNull Exception exception) {
-                //이미지 로드 실패시
-                Toast.makeText(getApplicationContext(), "실패", Toast.LENGTH_SHORT).show();
+            public void onComplete(@NonNull Task<Uri> task) {
+                if(task.isSuccessful()){
+                    // Glide 이용하여 이미지뷰에 로딩
+                    Glide.with(MainActivity.this)
+                            .load(task.getResult())
+                            .into(imgview_snatural);
+                }else{
+                    // URL을 가져오지 못하면 토스트 메세지
+                    Toast.makeText(MainActivity.this, "오류", Toast.LENGTH_SHORT).show();  }
             }
         });
+
+//        StorageReference storageRef_studentN = storage.getReference("natural/20201110055413_natural_student.jpg");
+//        StorageReference storageRef_staffN = storage.getReference("natural/20201110055413_natural_staff.jpg");
+
+//        StorageReference storageRef_studentL = storage.getReference("liberal/20201116021728_liberal_student.jpg");
+//        StorageReference storageRef_staffL = storage.getReference("liberal/20201116021728_liberal_staff.jpg");
+
 
 
         txtview_day.setText(getTime);
 
-        imgview_sliberal.setVisibility(View.VISIBLE);
         imgview_snatural.setVisibility(View.VISIBLE);
-
-
-//                myRef.addValueEventListener(new ValueEventListener() {
-//                    @Override
-//                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-//                        st_view = snapshot.getValue().toString();
-//                        textView.setText(st_view);
-//                    }
-//
-//                    @Override
-//                    public void onCancelled(@NonNull DatabaseError error) {
-//                        st_view = "Database Error";
-//                    }
-//                }
+        imgview_stfnatural.setVisibility(View.VISIBLE);
+//        imgview_sliberal.setVisibility(View.INVISIBLE);
+//        imgview_stfliberal.setVisibility(View.INVISIBLE);
 
     }
-
-
 }
